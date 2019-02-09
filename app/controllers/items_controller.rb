293ee2c5a7559c22113e37e6426bc5c07662c78a
raminyou2 <raminyou3@gmail.com>
2019-02-09 17:28:36 +0900

    編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,15 @@ class ItemsController < ApplicationController
     @item.save
     redirect_to @item
   end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+
+  end
   private 
   def item_params
     params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
